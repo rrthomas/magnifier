d4ec1c74d780f8bbc97e01b30f95b27264455a98
Reuben Thomas <rrt@sc3d.org>
2011-08-03 17:30:49 +0100

    Fix FIXME; bump to 2.3.

--- magnifier.jquery.js
+++ magnifier.jquery.js
@@ -1,6 +1,6 @@
 /*
  Magnifier: a jQuery Plugin (tested with 1.6.2)
- Version 2.2
+ Version 2.3
  (c) Reuben Thomas 2011
  This code is in the public domain.
 
@@ -10,47 +10,41 @@
  */
 
 (function($) {
-     $.magnifier = { defaults: { mover: "mover" } };
+    $.magnifier = { defaults: { mover: "mover" } };
 
-     $.fn.extend({magnifier:function(config, small, large) {
-                      config = $.extend({}, $.magnifier.defaults, config);
-                      this.html(
-                                '<div style="position: relative"><img src="'+small+'" />' +
-                                '<div id="'+config.mover+'"></div></div>'
-                                );
+    $.fn.extend({magnifier:function(config, small, large) {
+        config = $.extend({}, $.magnifier.defaults, config);
+        this.html(
+            '<div style="position: relative"><img src="'+small+'" />' +
+                '<div id="'+config.mover+'"></div></div>'
+        );
 
-                      var smallArea = $(':first-child', this);
-                      var mover = $("#"+config.mover);
-                      mover.css({ backgroundImage: "url('"+large+"')", backgroundRepeat: "no-repeat" });
+        var smallArea = $(':first-child', this);
+        var mover = $("#"+config.mover);
+        mover.css({ backgroundImage: "url('"+large+"')", backgroundRepeat: "no-repeat" });
 
-                      var largeImg = new Image();
-                      largeImg.src = large;
+        var largeImg = new Image();
+        largeImg.src = large;
 
-                      $(window).load(function () {
-                                         var x_offset = mover.width() / 2;
-                                         var y_offset = mover.height() / 2;
+        $(window).load(function () {
+            var x_offset = mover.width() / 2;
+            var y_offset = mover.height() / 2;
 
-                                         smallArea
-                                             .mousemove(function (e) {
-                                                            var x = e.pageX - $("img", smallArea).offset().left;
-                                                            var y = e.pageY - $("img", smallArea).offset().top;
+            smallArea
+                .mousemove(function (e) {
+                    var x = e.pageX - $("img", smallArea).offset().left;
+                    var y = e.pageY - $("img", smallArea).offset().top;
 
-                                                            // FIXME: Should be able to use hover event
-                                                            if (x < 0 || x > $("img", smallArea).width() || y < 0 || y > $("img", smallArea).height()) {
-                                                                mover.hide();
-                                                            } else {
-                                                                mover.show();
-                                                                mover.css({ left: x - x_offset, top: y - y_offset,
-                                                                            backgroundPosition: "" +
-                                                                            -(x * largeImg.width / $("img", smallArea).width() - x_offset) + "px " +
-                                                                            -(y * largeImg.height / $("img", smallArea).height() - y_offset) + "px"
-                                                                          });
-                                                            }
-                                                        });
+                    mover.css({ left: x - x_offset, top: y - y_offset,
+                                backgroundPosition: "" +
+                                -(x * largeImg.width / $("img", smallArea).width() - x_offset) + "px " +
+                                -(y * largeImg.height / $("img", smallArea).height() - y_offset) + "px"
+                              });
+                })
+                .mouseover(function (e) { mover.show(); })
+                .mouseout(function (e) { mover.hide(); });
+        });
 
-                                     });
-
-                      return this;
-                  }
-                 });
- })(jQuery);
+        return this;
+    }});
+})(jQuery);
