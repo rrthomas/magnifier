df6230194904e9acea5754216b9c09ec8a81b478
Reuben Thomas <rrt@sc3d.org>
2011-08-13 10:44:05 +0100

    Rewrite again: 2.4 uses API of 2.3, but with display technique of 2.0.

--- magnifier.jquery.js
+++ magnifier.jquery.js
@@ -1,28 +1,30 @@
 /*
- Magnifier: a jQuery Plugin (tested with 1.6.2)
- Version 2.3
- (c) Reuben Thomas 2011
- This code is in the public domain.
+  Magnifier: a jQuery Plugin (tested with 1.6.2)
+  Version 2.4
+  (c) Reuben Thomas 2011
+  This code is in the public domain.
 
- Based on AnythingZoomer version 1.0 by Chris Coyier
- http://css-tricks.com/anythingzoomer-jquery-plugin/
- License: "Note: You can do whatever the heck you want with this."
- */
+  Based on AnythingZoomer version 1.0 by Chris Coyier
+  http://css-tricks.com/anythingzoomer-jquery-plugin/
+  License: "Note: You can do whatever the heck you want with this."
+*/
 
 (function($) {
     $.magnifier = { defaults: { mover: "mover" } };
 
     $.fn.extend({magnifier:function(config, small, large) {
         config = $.extend({}, $.magnifier.defaults, config);
-        this.html(
-            '<div style="position: relative"><img src="'+small+'" />' +
-                '<div id="'+config.mover+'"></div></div>'
+        var wrap = this;
+        wrap.html(
+            '<div style="position: relative;"><img src="'+small+'" /></div>' +
+                '<div id="'+config.mover+'"><div style="position: absolute;"><img src="'+large+'" /></div></div>'
         );
-
-        var smallArea = $(':first-child', this);
+        var smallArea = $(':first-child', wrap);
         var mover = $("#"+config.mover);
-        mover.css({ backgroundImage: "url('"+large+"')", backgroundRepeat: "no-repeat" });
+        var largeArea = $(':first-child', mover);
 
+        var smallImg = new Image();
+        smallImg.src = small;
         var largeImg = new Image();
         largeImg.src = large;
 
@@ -30,16 +32,22 @@
             var x_offset = mover.width() / 2;
             var y_offset = mover.height() / 2;
 
-            smallArea
+            wrap
+                .width(smallImg.width)
+                .height(smallImg.height)
                 .mousemove(function (e) {
-                    var x = e.pageX - $("img", smallArea).offset().left;
-                    var y = e.pageY - $("img", smallArea).offset().top;
+                    var x = e.pageX - smallArea.offset().left;
+                    var y = e.pageY - smallArea.offset().top;
 
-                    mover.css({ left: x - x_offset, top: y - y_offset,
-                                backgroundPosition: "" +
-                                -(x * largeImg.width / $("img", smallArea).width() - x_offset) + "px " +
-                                -(y * largeImg.height / $("img", smallArea).height() - y_offset) + "px"
-                              });
+                    mover.css({
+                        left: x - x_offset,
+                        top: y - y_offset
+                    });
+
+                    largeArea.css({
+                        left: -(x * largeImg.width / smallImg.width - x_offset),
+                        top: -(y * largeImg.height / smallImg.height - y_offset),
+                    });
                 })
                 .mouseover(function (e) { mover.show(); })
                 .mouseout(function (e) { mover.hide(); });
